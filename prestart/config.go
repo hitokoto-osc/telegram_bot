a1ce8dfe24888089df03a1642bf8adbb671f6a2c
Jonson Petard <41122242+greenhat616@users.noreply.github.com>
2023-09-14 22:13:40 +0800

    chore(config): reading env vars

--- prestart/config.go
+++ prestart/config.go
@@ -19,8 +19,6 @@ func initConfig(path string) {
 	viper.SetDefault("telegram.poll_interval", 1)
 
 	logger.Debug("开始读取配置文件...")
-	viper.SetEnvPrefix("TELEGRAM_BOT_")
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 	viper.SetConfigName("config")
 	viper.AddConfigPath(".") // 二进制执行目录
 	viper.AddConfigPath("./config")
@@ -37,6 +35,11 @@ func initConfig(path string) {
 			logger.Warn("配置文件不存在，使用默认配置（或从环境变量读取）", zap.Error(err))
 		}
 	}
+	// 读取环境变量
+	viper.SetEnvPrefix("TELEGRAM_BOT_")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
+	
 	logger.Debug(
 		"已加载配置文件",
 		zap.String("config_file_used", viper.ConfigFileUsed()),
